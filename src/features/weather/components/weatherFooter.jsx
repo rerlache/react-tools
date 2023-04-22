781ec73ca6746879bdb386a18836d69680a41eba
rerlache <rerlache@gmail.com>
2023-04-22 20:40:44 +0200

    weater responiveness finished

--- src/features/weather/components/weatherFooter.jsx
+++ src/features/weather/components/weatherFooter.jsx
@@ -5,7 +5,7 @@ export default function WeatherFooter({data}) {
   return (
     <div className={`${Style["footer-container"]}`}>
       <div className={`${Style['footer-info']}`}>
-        <div className={`${Style['info-value']}`}>{Math.round(data.main.feels_like)}&deg;</div>
+        <div className={`${Style['info-value']}`}>{Math.round(data.main.feels_like)}&deg;C</div>
         <div className={`${Style['info-desc']}`}>Feels Like</div>
       </div>
       <div className={`${Style['footer-info']}`}>
--- src/features/weather/components/weatherInformation.jsx
+++ src/features/weather/components/weatherInformation.jsx
@@ -7,20 +7,26 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("de-AT", {
 });
 
 export default function WeatherInformation({ data, forecast }) {
-  const formatedPopulation = INTEGER_FORMATTER.format(forecast.city.population)
+  const formatedPopulation = INTEGER_FORMATTER.format(forecast.city.population);
   return (
     <div className={`${Style["weather-information"]}`}>
-      <label htmlFor="location-input">{forecast.city.name}, {forecast.city.country}</label>
-      <div>({forecast.city.coord.lat} - {forecast.city.coord.lon})</div>
-    <div>Sunrise: {dayjs.unix(forecast.city.sunrise).format('HH:mm')}</div>
-    <div>Sunset: {dayjs.unix(forecast.city.sunset).format('HH:mm')}</div>
-    <div>population: {formatedPopulation}</div>
+      <label htmlFor="location-input">
+        {forecast.city.name}, {forecast.city.country}
+      </label>
+      <div>
+        ({forecast.city.coord.lat} - {forecast.city.coord.lon})
+      </div>
+      <div>Sunrise: {dayjs.unix(forecast.city.sunrise).format("HH:mm")}</div>
+      <div>Sunset: {dayjs.unix(forecast.city.sunset).format("HH:mm")}</div>
+      <div>population: {formatedPopulation}</div>
       <div className={`${Style["temp"]}`}>
         {Math.round(data.main.temp)}&deg;C
       </div>
       <div className={`${Style["weather-icon"]}`}>
         <div className={`${Style["description"]}`}>{data.weather[0].main}</div>
-        <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
+        <img
+          src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        />
       </div>
     </div>
   );
