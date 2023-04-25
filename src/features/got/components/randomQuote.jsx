53408c247179bcbc418d6d5e6c054ed8d5199516
rerlache <rerlache@gmail.com>
2023-04-25 19:59:14 +0200

    refined styling of GoT

--- src/features/got/components/randomQuote.jsx
+++ src/features/got/components/randomQuote.jsx
@@ -12,24 +12,31 @@ export default function RandomQuote() {
   }, []);
 
   async function fetchRandomQuote() {
-    if (randomQuote === undefined) {
-      const response = await axios.get(URL);
-      if (response.status === 200) {
-        console.log(response.data);
-        setRandomQuote(response.data);
-      }
+    const response = await axios.get(URL);
+    if (response.status === 200) {
+      console.log(response.data);
+      setRandomQuote(response.data);
     }
   }
 
   return (
-    <div className={`${Style["got-font"]} ${Style["sub-header"]}`}>
-      Random Quote:
+    <>
+      <div className={`${Style["got-font"]} ${Style["sub-header"]}`}>
+        Random Quote:
+      </div>
       {randomQuote && (
         <div className={`${Style["quote-container"]}`}>
+          <button
+            onClick={() => fetchRandomQuote()}
+            className={`${Style["quote-btn"]}`}
+          >
+            Get Quote
+          </button>
+
           <div className={`${Style["quote"]}`}>{randomQuote.sentence}</div>
           <div className={`${Style["from"]}`}>{randomQuote.character.name}</div>
         </div>
       )}
-    </div>
+    </>
   );
 }
